a7ad4713f2cdfed43fd856a78955bde316219729
Richard Hull <rm_hull@yahoo.co.uk>
2025-08-24 15:51:38 +0100

    Empty Xcode project

---
+++ clipboard-manager/clipboard_managerApp.swift
@@ -0,0 +1,17 @@
+//
+//  clipboard_managerApp.swift
+//  clipboard-manager
+//
+//  Created by Richard Hull on 24/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct clipboard_managerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
